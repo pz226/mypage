c9d2998c05857dd1b43c3d1a575ae2f223ecfad3
pz226 <43305998+pz226@users.noreply.github.com>
2019-04-29 20:06:00 +0900

    deltaのパロディページ

--- sub.js
+++ sub.js
@@ -1,11 +1,11 @@
 (function () {
     $(window).scroll(function () {
-        var Num = $(window).scrollTop() / 500;
-        var Num2 = $(window).scrollTop() * .0004; // higher number for more zoom
-        var Num2mod = Num2 + 1;
-        var Num3 = $(window).scrollTop() * .2; // Title speed
-        var Num3mod = Num3 + 1;
-        return $('.shade').css('opacity', Num),
+       // var Num = $(window).scrollTop() / 500;
+       // var Num2 = $(window).scrollTop() * .0004; // higher number for more zoom
+       // var Num2mod = Num2 + 1;
+       // var Num3 = $(window).scrollTop() * .2; // Title speed
+       // var Num3mod = Num3 + 1;
+       // return $('.shade').css('opacity', Num),
             $(".bg").css({"transform":"scale(" + Num2mod + ")"}),
             $(".text").css({"margin-top":"-" + Num3mod + "px"});
     });
